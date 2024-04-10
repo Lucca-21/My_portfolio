b6ec749a70ad11ff0107b07d9995d8298baa2039
Lucca Rodrigues <lucca21.lucca21@gmail.com>
2024-04-10 20:38:30 -0300

    add humburger

---
+++ Estilização/myscript.js
@@ -0,0 +1,14 @@
+/* ==================== Send Email ================================= */
+
+function SendMail() {
+    var params = {
+        from_name : document.getElementById('name_input').value,
+        email_id : document.getElementById('email_input').value,
+        message : document.getElementById('message_input').value
+    }
+
+    emailjs.send('service_gk2sanu', 'template_xic9z5f', params).then(function (res) {
+        alert('Success!' + res.status);
+    })
+}
+ 
---
+++ Estilização/myscript_body.js
@@ -0,0 +1,36 @@
+/* ================= Hamburger ================ */
+
+const hamburger = document.querySelector('button#hb_button')
+const nav = document.querySelector('nav')
+const hb_img = document.querySelector('img#hb_open')
+
+hamburger.addEventListener ('click', clicou)
+
+function clicou() {
+    hamburger.classList.toggle('active')
+    nav.classList.toggle('active')
+
+    if (hb_img.getAttribute('src') !== '/imagens/close.svg') {
+        hb_img.setAttribute('src', '/imagens/close.svg')
+    } else {
+        hb_img.setAttribute('src', '/imagens/open.svg')
+    }
+
+    const about = document.querySelector('#about_nav')
+    const overview = document.querySelector('#overview_nav')
+    const contact = document.querySelector('#contact_nav')
+    
+    about.addEventListener ('click', clicou2)
+    overview.addEventListener ('click', clicou2)
+    contact.addEventListener ('click', clicou2)
+
+    function clicou2() {
+        hamburger.classList.remove('active')
+        nav.classList.remove('active')
+
+        if (hb_img.getAttribute('src') === '/imagens/close.svg') {
+            hb_img.setAttribute('src', '/imagens/open.svg')
+        } else {
+        }
+    }
+}
